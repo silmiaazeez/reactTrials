f027d09905e04e9ed4240151c1a1b2ebddece0bb
silmiaazeez <miyaraaz@gmail.com>
2024-02-18 12:45:16 +0300

    helloworld

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import HelloWorld from './HelloWorld';
 
 function App() {
   return (
@@ -17,6 +18,8 @@ function App() {
         >
           Learn React
         </a>
+
+        <HelloWorld />
       </header>
     </div>
   );
